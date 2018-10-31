fe77869397d9c86cda2411513f648d052c18aaf8
Elliott Slaughter <slaughter@cs.stanford.edu>
2018-10-31 11:34:39 -0700

    Initial Travis configuration.

---
+++ test_all.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -e
+set -x
+
+LUA_VERSION=${LUA_VERSION:-5.2.4}
+export LUA_PREFIX=$PWD/install
+if [[ $(uname) == Linux ]]; then
+    LUA_TARGET=linux
+elif [[ $(uname) == Darwin ]]; then
+    LUA_TARGET=macosx
+else
+    LUA_TARGET=posix
+fi
+
+if [[ ! -d $LUA_PREFIX ]]; then
+    mkdir $LUA_PREFIX
+
+    curl -o lua-$LUA_VERSION.tar.gz https://www.lua.org/ftp/lua-$LUA_VERSION.tar.gz
+    tar xfz lua-$LUA_VERSION.tar.gz
+    rm lua-$LUA_VERSION.tar.gz
+
+    pushd lua-$LUA_VERSION
+    make $LUA_TARGET install INSTALL_TOP="$LUA_PREFIX"
+    popd
+fi
+
+export PATH="$PATH:$LUA_PREFIX/bin"
+
+make all
+make test
